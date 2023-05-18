2678e207bb5c6e74a1c542de58bdf5eb0cf95574
Brayden Jonsson <brayden.f.jonsson@gmail.com>
2023-05-17 18:15:42 -0600

    Added an education button

---
+++ src/education.vue
@@ -0,0 +1,43 @@
+<script setup>
+import DocumentationIcon from "@/components/icons/IconDocumentation.vue";
+import SummaryItem from "@/components/summary-item.vue";
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <SummaryItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Education</template>
+
+      I graduated from <a href="https://lphs.alpineschools.org/">Lone Peak High School</a> in 2022, where
+      I was recognized as Salutatorian. I also was recognized as the Computer Technology <a href="http://www.sterlingscholar.org/">
+      Sterling Scholar</a>, and went on to be recognized as a finalist in the Wasatch Front region. I also
+      received the Distinguished Student Award from the BYU Math Department.
+      <br>
+      I currently attend the University of Utah, pursuing an Honors Computer Science BS with a Math minor
+      and a Physics minor. My expected graduation date is Fall 2025, but I also plan to pursue the BS/MS
+      program with a graduation date of Spring 2027. I participate in the Engineering Scholars program, as
+      well as in <a href="https://getinvolved.utah.edu/organization/gamecraft">Gamecraft</a> Game Jams.
+    </SummaryItem>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: normal;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+}
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,10 +1,15 @@
 <script setup>
-import Introduction from './components/Introduction.vue'
-import Summaries from './components/Summaries.vue'
+import Introduction from './components/index-introduction.vue'
+import Summaries from './components/index-summaries.vue'
+import Button from "./components/button.vue";
+
+import { ref } from "vue";
+const education = ref("education.html");
 </script>
 
 <template>
   <header>
+    <Button :href="education"> Education </Button>
     <div class="wrapper">
       <Introduction />
     </div>
@@ -20,20 +25,11 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: normal;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
